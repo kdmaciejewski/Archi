200786dc6558fbf114f9c8f64f150dc377a8995b
Krzysztof Maciejewski <94606867+kdmaciejewski@users.noreply.github.com>
2024-10-10 15:15:09 +0200

    adding guards

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Button } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [Button],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  private router = inject(Router);
+  logout() {
+    sessionStorage.clear();
+    this.router.navigate(['login']);
+  }
 }
